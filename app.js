05cd6a3a849f6b8c7c5658c35b7fcead619699c0
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-28 17:25:23 +0200

    rajout de for

---
+++ app.js
@@ -0,0 +1,35 @@
+for (var i = 0; i >= 20; i ++) {
+
+	$('<div class="debris"/>').appendTo('#jeu');
+
+	
+
+	
+};
+
+
+
+
+
+
+
+
+
+//$( "#" ).click(function() {
+//  $( "#" ).mousedown();
+//});
+
+//$( "#" ).click(function() {
+//  $( "#" ).mouseup();
+//});
+
+
+
+
+
+
+
+
+
+
+
